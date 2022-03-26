81429eb38fa5665eeb56856ef1e633defd62c8d2
James Murphy <jamesivdeveloper@gmail.com>
2022-03-26 18:20:50 -0500

    add helper functions with partials

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,28 @@
+// login form handler
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
+      method: "post",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: { "Content-Tyoe": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
 // signup form handler
 async function signupFormHandler(event) {
   event.preventDefault();
@@ -15,31 +40,6 @@ async function signupFormHandler(event) {
         password,
       }),
       headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok) {
-      document.location.replace("/");
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
-
-// login form handler
-async function loginFormHandler(event) {
-  event.preventDefault();
-
-  const email = document.querySelector("#email-login").value.trim();
-  const password = document.querySelector("#password-login").value.trim();
-
-  if (email && password) {
-    const response = await fetch("/api/users/login", {
-      method: "post",
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-      headers: { "Content-Tyoe": "application/json" },
     });
 
     if (response.ok) {
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
 const path = require("path");
-const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const express = require("express");
 const session = require("express-session");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const routes = require("./controllers");
 
 const sess = {
   secret: "Super secret secret",
@@ -20,13 +20,17 @@ const sess = {
   }),
 };
 
+app.use(session(sess));
+
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({ helpers });
+
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(session(sess));
 
 app.use(routes);
 
